8dfe93cbd44f73edb4c8ff06294924f122bc9c61
Eric E <71135897+EricE-Freelancer@users.noreply.github.com>
2020-09-14 16:31:03 +0800

    Create whats_your_name.py

---
+++ whats_your_name.py
@@ -0,0 +1,7 @@
+print("Hi! Please Enter your name:")  #user will request to enter name
+x = input()                           #variable x for the input
+print("Hello, " + x)                  #print Hello + the value of the input in var x
+print("Welcome to Python")
+print ("Current Date and Time")
+from datetime import datetime         #additional item to call date and time
+print(datetime.now())
