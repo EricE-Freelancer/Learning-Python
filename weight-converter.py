a0ceaa09afead8082d95580c8991cf8c8e81c5e7
Eric E <71135897+EricE-Freelancer@users.noreply.github.com>
2020-09-14 16:35:32 +0800

    Add files via upload

---
+++ weight-converter.py
@@ -0,0 +1,11 @@
+#Weight converter
+Weight = int (input('Weight: '))
+unit = input ('(L)bs or (K)g: ')
+if unit.upper() == "L":
+    converted = Weight*0.45
+    print(f"You are {converted} kilos")
+else:
+    converted = Weight / 0.45
+    print(f"you are {converted} pounds")
+
+
